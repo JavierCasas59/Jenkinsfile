aa15119578565e1c139969852c0ba442867d3e1e
PPS13228313 <javcasari@alu.edu.gva.es>
2026-04-28 21:16:45 +0200

    Update calculadora.py
    cambio

--- calculadora.py
+++ calculadora.py
@@ -1,16 +1,19 @@
+import sys
+
 class Calculadora:
     def multiplicar(self, a, b):
         return a * b
 
+
 if __name__ == "__main__":
     try:
-        num1 = int(input("Introduce el primer número: "))
-        num2 = int(input("Introduce el segundo número: "))
+        num1 = int(sys.argv[1])
+        num2 = int(sys.argv[2])
 
         calc = Calculadora()
         resultado = calc.multiplicar(num1, num2)
 
         print(f"Resultado: {resultado}")
 
-    except ValueError:
-        print("Error: debes introducir números válidos")
+    except (IndexError, ValueError):
+        print("Uso correcto: python calculadora.py <num1> <num2>")
